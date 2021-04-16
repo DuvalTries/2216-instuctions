050544a577d29f5cbc28d808bed2aaf6adef4d8e
DuvalTries <72459320+DuvalTries@users.noreply.github.com>
2021-04-15 20:06:50 -0700

    Update firebase_api_marco.js

--- scripts/firebase_api_marco.js
+++ scripts/firebase_api_marco.js
@@ -6,13 +6,6 @@
 var firebaseConfig = {
     
     // Your API stuff goes here;  get it from firebase console
-    apiKey: "AIzaSyAZZbYg_rrFWuMPWUx4NXQ7TMka3-zmdLA",
-    authDomain: "mango-76400.firebaseapp.com",
-    databaseURL: "https://mango-76400.firebaseio.com",
-    projectId: "mango-76400",
-    storageBucket: "mango-76400.appspot.com",
-    messagingSenderId: "124942820208",
-    appId: "1:124942820208:web:0249f52a34f1cef2a925b5"
     
   };
   
